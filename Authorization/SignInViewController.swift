a70acb8ff317adb96441979792cc4493f54d7d22
Vadim <103464053+Branios@users.noreply.github.com>
2022-05-19 15:15:09 +0300

    invalid user data alerts

--- Authorization/SignInViewController.swift
+++ Authorization/SignInViewController.swift
@@ -12,16 +12,31 @@ class SignInViewController: UIViewController {
     @IBOutlet var usernameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     
+    private var username = "username"
+    private var password = "password"
     
     
     
-    // something wrong
+    // Alerts for invalid user data or empty text fields
     
     @IBAction func doneButtonPressed() {
-        guard let inputText = usernameTextField.text, !inputText.isEmpty else {
-            print("!")
-            return
+        if username != username && username .isEmpty || password != password && password .isEmpty {
+            showAlert(title: "Oops...", message: "Invalid username or password")
         }
+        
+        // check that username has not numbers
+        
+        if username == Double(username) {
+            showAlert(title: "Oops...", message: "Do not use numbers in username field")
+        }
+    }
+    
+    
+    // Alerts private funcion
+    
+    private func showAlert (title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Ok", style: .default)
     }
     
 }
